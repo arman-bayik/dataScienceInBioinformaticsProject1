7caf040a50bcf6579a0a55a525789bd13264ebcc
Arman Bayik <arman.bayik@ufl.edu>
2020-03-07 16:00:56 -0500

    reformatted sequenceGenerator.py

--- sequenceGenerator.py
+++ sequenceGenerator.py
@@ -16,67 +16,67 @@ import random
 
 
 def main():
-#  -------------------INPUT VERIFICATION AND RECORDING-------------------
-    if(len(sys.argv) < 9): # Verifying there aren't too few inputs
+    #  -------------------INPUT VERIFICATION AND RECORDING-------------------
+    if len(sys.argv) < 9:  # Verifying there aren't too few inputs
         print("Error: Too few inputs.\n")
         print("Inputs should be in the form of [run program] n a c g t k p \"o\"")
         sys.exit()
-    if(len(sys.argv) > 9):  # Verifying there aren't too many inputs
+    if len(sys.argv) > 9:  # Verifying there aren't too many inputs
         print("Error: Too many inputs.\n")
         print("Inputs should be in the form of [run program] n a c g t k p \"o\"")
         sys.exit()
     try:  # Verifying input of n is correct and storing it in variable n
         n = int(sys.argv[1])
-        if(n < 0):
-            raise(ValueError)
+        if n < 0:
+            raise ValueError
     except ValueError:
         print("Error: input for 'n' must be positive and of type int.")
         sys.exit()
 
     try:  # Verifying input of a is correct and storing it in variable a
         a = int(sys.argv[2])
-        if(a < 0):
-            raise(ValueError)
+        if a < 0:
+            raise ValueError
     except ValueError:
         print("Error: input for 'a' must be positive and of type int.")
         sys.exit()
 
     try:  # Verifying input of c is correct and storing it in variable c
         c = int(sys.argv[3])
-        if(c < 0):
-            raise(ValueError)
+        if c < 0:
+            raise ValueError
     except ValueError:
         print("Error: input for 'c' must be positive and of type int.")
         sys.exit()
 
     try:  # Verifying input of g is correct and storing it in variable g
         g = int(sys.argv[4])
-        if(g < 0):
-            raise(ValueError)
+        if g < 0:
+            raise ValueError
     except ValueError:
         print("Error: input for 'g' must be positive and of type int.")
         sys.exit()
 
     try:  # Verifying input of t is correct and storing it in variable t
         t = int(sys.argv[5])
-        if(t < 0):
-            raise(ValueError)
+        if t < 0:
+            raise ValueError
     except ValueError:
         print("Error: input for 't' must be positive and of type int.")
         sys.exit()
 
     try:  # Verifying input of k is correct and storing it in variable k
         k = int(sys.argv[6])
-        if(k < 0):
-            raise(ValueError)
+        if k < 0:
+            raise ValueError
     except ValueError:
         print("Error: input for 'k' must be positive and of type int.")
         sys.exit()
 
     try:  # Verifying input of p is correct and storing it in variable p
         p = float(sys.argv[7])
-        if(p < 0 or p > 1):
-            raise(ValueError)
+        if (p < 0) or (p > 1):
+            raise ValueError
     except ValueError:
         print("Error: input for 'p' must be in the range [0,1] (inclusive) and of type double.")
         sys.exit()
@@ -90,35 +90,35 @@ def main():
     # Calculating s to generate random nucleotides, a c g and t will be valid if this point is reached
     s = a + c + g + t
     # Calculating probabilities and bounds to generate a c g and t nucleotides using a random decimal generator
-    aProb = float(a)/float(s)
-    cProb = float(c)/float(s)
-    gProb = float(g)/float(s)
-    tProb = float(t)/float(s)
-    aLB = 0
-    aUB = aProb
-    cLB = aUB
-    cUB = aUB + cProb
-    gLB = cUB
-    gUB = cUB + gProb
-    tLB = gUB
-    tUB = 1
-    aBounds = [aLB, aUB]  # >=, <
-    cBounds = [cLB, cUB]  # >=, <
-    gBounds = [gLB, gUB]  # >=, <
-    tBounds = [tLB, tUB]  # >=, <=
+    a_prob = float(a)/float(s)
+    c_prob = float(c)/float(s)
+    g_prob = float(g)/float(s)
+    t_prob = float(t)/float(s)
+    a_lb = 0
+    a_ub = a_prob
+    c_lb = a_ub
+    c_ub = a_ub + c_prob
+    g_lb = c_ub
+    g_ub = c_ub + g_prob
+    t_lb = g_ub
+    t_ub = g_ub + t_prob
+    a_bounds = [a_lb, a_ub]  # >=, <
+    c_bounds = [c_lb, c_ub]  # >=, <
+    g_bounds = [g_lb, g_ub]  # >=, <
+    t_bounds = [t_lb, t_ub]  # >=, <=
     # Creating the output file and writing sequence information to it
     out = open(o, "w")
-    for i in range(0,k,1):
+    for i in range(0, k, 1):
         out.write(">\n")
-        for j in range(0,n,1):
-            nucleotideSelect = random.uniform(0,1)
-            if(nucleotideSelect >= aLB and nucleotideSelect < aUB):
+        for j in range(0, n, 1):
+            nucleotide_select = random.uniform(0, 1)
+            if(nucleotide_select >= a_bounds[0]) and (nucleotide_select < a_bounds[1]):
                 out.write("A")
-            elif(nucleotideSelect >= cLB and nucleotideSelect < cUB):
+            elif(nucleotide_select >= c_bounds[0]) and (nucleotide_select < c_bounds[1]):
                 out.write("C")
-            elif(nucleotideSelect >= gLB and nucleotideSelect < gUB):
+            elif(nucleotide_select >= g_bounds[0]) and (nucleotide_select < g_bounds[1]):
                 out.write("G")
-            elif(nucleotideSelect >= tLB and nucleotideSelect <= tUB):
+            elif(nucleotide_select >= t_bounds[0]) and (nucleotide_select <= t_bounds[1]):
                 out.write("T")
         out.write("\n")
     out.close()
--- sequencePartitioner.py
+++ sequencePartitioner.py
@@ -7,8 +7,20 @@
 
 # Sample call: python sequencePartitioner.py "input.txt" 6 20 "output.txt"
 
+import sys
+
 
 def main():
+    #  -------------------INPUT VERIFICATION AND RECORDING-------------------
+    if (len(sys.argv) < 9):  # Verifying there aren't too few inputs
+        print("Error: Too few inputs.\n")
+        print("Inputs should be in the form of [run program] n a c g t k p \"o\"")
+        sys.exit()
+    if (len(sys.argv) > 9):  # Verifying there aren't too many inputs
+        print("Error: Too many inputs.\n")
+        print("Inputs should be in the form of [run program] n a c g t k p \"o\"")
+        sys.exit()
+
     print("Program run completed successfully.")
     return 0
 
