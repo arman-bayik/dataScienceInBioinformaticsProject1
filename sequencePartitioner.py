345cf2379ffba54f6d7893f48d9e6a6431b018d9
Arman Bayik <arman.bayik@ufl.edu>
2020-03-07 10:27:10 -0500

    set up sequence partitioner

---
+++ sequencePartitioner.py
@@ -0,0 +1,17 @@
+# Simulates DNA sequence partitioning
+# Inputs:
+#      string i: input file name
+#      int x : minimum fragment length
+#      int y : maximum fragment length
+#      string o : output file name
+
+# Sample call: python sequencePartitioner.py "input.txt" 6 20 "output.txt"
+
+
+def main():
+    print("Program run completed successfully.")
+    return 0
+
+
+if __name__ == '__main__':
+    main()
